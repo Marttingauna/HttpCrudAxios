09dd2accd3c98dba9162a64a87f6e995a8f18993
Martín <marrtingauna@gmail.com>
2022-06-03 20:11:03 -0300

    🚚 Creando pipe y mostrando respuesta en la vista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListaComponent } from './component/lista/lista.component';
 import { AgregarComponent } from './component/agregar/agregar.component';
-
+import { KeysPipe } from "./pipes/keys.pipe";
 @NgModule({
   declarations: [
     AppComponent,
     ListaComponent,
-    AgregarComponent
+    AgregarComponent,
+    KeysPipe,
   ],
   imports: [
     BrowserModule
--- src/app/component/lista/lista.component.ts
+++ src/app/component/lista/lista.component.ts
@@ -8,10 +8,16 @@ import { AxiosService } from '../../services/axios.services';
 })
 export class ListaComponent implements OnInit {
 
+  datos:any ={};
+
   constructor(private servicio: AxiosService) { }
 
   ngOnInit(): void {
-    this.servicio.getPersonas()
+    this.servicio.getPersonas().then(
+      (resp)=>{
+        this.datos = resp;
+      }
+    )
   }
 
 }
