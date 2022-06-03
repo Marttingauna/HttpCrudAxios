746d0c1f4ca6579f776e1384e3a0e6eeee2fcaf4
Martín <marrtingauna@gmail.com>
2022-06-03 18:25:50 -0300

    🎈 Creando componentes para agregar, listar personas  e interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaComponent } from './component/lista/lista.component';
+import { AgregarComponent } from './component/agregar/agregar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    AgregarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/interface/persona.interface.ts
@@ -0,0 +1,7 @@
+export interface Persona {
+    key$: String;
+    nombre: String;
+    lugar: String;
+    fecha: String;
+    descripcion: String;
+}
